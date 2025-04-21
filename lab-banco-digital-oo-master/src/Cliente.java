c3e2887a30d636614b2341165f972cb72c69153f
Rayan <141056772+Rayan757575@users.noreply.github.com>
2025-04-21 12:28:28 -0300

    Add files via upload

---
+++ lab-banco-digital-oo-master/src/Cliente.java
@@ -0,0 +1,24 @@
+
+public class Cliente {
+
+	private String nome;
+	private Banco banco;
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}	
+	
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+
+}
